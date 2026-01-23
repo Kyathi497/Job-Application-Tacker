84e6737b1e0796d4ce84a0b29e94cdfdfcc07cd8
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2026-01-23 06:41:21 +0530

    initial commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import {ArrowRight} from 'lucide-react'
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <main className="flex-1">
+        {/* Hero section */}
+        <section className="container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">A better way to track your job application.</h1>
+            <p className="text-muted-foreground mb-10 text-xl">capture, organize and manae your job search in one place.</p>
+            <div className="flex flex-col items-center gap-4">
+              <Link href="/sign-up">
+                <Button size="lg" className="h-12 px-8 text-lg bg-primary font-medium">
+                Start for free <ArrowRight/>
+              </Button>
+              </Link>
+              <p className="text-sm text-muted-foreground">Free forever, No credit card required.</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}

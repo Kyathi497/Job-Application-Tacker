53022744b0a508bf1bac81017384f016a39e3d6a
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2026-01-24 12:05:19 +0530

    db setup started still need to complete

---
+++ my-app/lib/db.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose"
+const MONGO_URL = process.env.MONGO_URL;
+
+
+interface MongooseCache {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+    var mongoose: MongooseCache | undefined;
+};
+
+let cached: MongooseCache = global.mongoose || {conn: null, promise: null};
+
+if (!global.mongoose) {
+    global.mongoose = cached;
+};
+
+async function connectDB() {
+    if (!MONGO_URL) {
+    throw new Error (
+        "please define the MONGO_URL environment variable inside .env"
+    );
+}
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false, 
+        }
+
+        cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
+            return mongoose;
+        })
+    }
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+    return cached.conn;
+}

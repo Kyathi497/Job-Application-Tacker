4241c0449da7c42d5cb46d1d910126c778ca313f
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2026-01-29 01:49:16 +0530

    changes in navbar and auth for getting the session from better-auth need to complete the navigation in navabar section after login

--- my-app/components/navbar.tsx
+++ my-app/components/navbar.tsx
@@ -1,8 +1,13 @@
 import { BriefcaseIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { getSession } from "@/lib/auth/auth";
+import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
 
-export default function Navbar() {
+
+export default async function Navbar() {
+    const session = await getSession();
     return (
         <nav className="border-b border-gray-200 bg-white">
             <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -10,7 +15,27 @@ export default function Navbar() {
                     <BriefcaseIcon/>
                     Job Tracker
                 </Link>
-                <div className="flex items-center gap-4">
+                
+                {session?.user ? (
+                    <>
+                        
+                            <Link href="/dashboard">
+                            <Button variant="ghost" className="text-gray-700 hover:text-black">Dashboard</Button>
+                        </Link>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Button variant="ghost" >
+                                    <Avatar>
+                                        <AvatarFallback className="bg-primary text-white">
+                                            {session.user.name[0].toUpperCase()}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </Button>
+                            </DropdownMenuTrigger>
+                        </DropdownMenu>
+                        
+                    </>
+                ) : <div className="flex items-center gap-4">
                     <Link href="/sign-in" >
                         <Button variant="ghost" className="text-gray-700 hover:text-black">
                             Log In
@@ -21,7 +46,7 @@ export default function Navbar() {
                             Start for free
                         </Button>
                     </Link>
-                </div>
+                </div>}
             </div>
 
         </nav>

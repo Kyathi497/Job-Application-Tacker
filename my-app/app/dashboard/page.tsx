b1e01b67f1743f5c3bb86c7c612a7acb2837552c
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2026-01-31 10:22:13 +0530

    models for the tables or collections are created and after sign up we created tables

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -3,7 +3,7 @@
 
 export default function Dashboard() {
     return(
-        <div className="flex items-center justify-end gap-5">
+        <div className="flex items-center justify-start">
             <p>Dashboard goes here..</p>
             
         </div>
--- my-app/components/sign-out-button.tsx
+++ my-app/components/sign-out-button.tsx
@@ -9,13 +9,29 @@ import { useRouter } from "next/navigation";
 export default function signOutButton() {
     const router = useRouter();
     return(
-        <DropdownMenuItem onClick={async () =>{
-            const result = await signOut();
-            if (result.data?.success) {
-                router.push('/sign-in')
-            } else {
-                alert("Error signing out..")
-            }
-        }}>Log Out</DropdownMenuItem>
+        <DropdownMenuItem
+  className="
+    flex items-center gap-2
+    text-sm font-medium
+    text-red-600
+    hover:bg-red-50
+    hover:text-red-700
+    rounded-md
+    px-3 py-2
+    transition-colors
+    pointer-fine::
+  "
+  onClick={async () => {
+    const result = await signOut()
+    if (result.data?.success) {
+      router.push('/sign-in')
+    } else {
+      alert('Error signing out..')
+    }
+  }}
+>
+  Log out
+</DropdownMenuItem>
+
     )
 }

0a2406fb5363aa6161f8c5fbab2973e7ec77f93c
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2026-02-02 19:35:40 +0530

    fixed mongoose issue in auth file

--- my-app/lib/auth/auth.ts
+++ my-app/lib/auth/auth.ts
@@ -1,11 +1,14 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { initializeUserBoard } from "../init-user-board";
+import connectDB from "../db";
 
-const client = new MongoClient(process.env.MONGO_URL!);
+
+
+const mongooseInstence = await connectDB();
+const client = mongooseInstence.connection.getClient();
 const db = client.db();
 
 export const auth = betterAuth({
